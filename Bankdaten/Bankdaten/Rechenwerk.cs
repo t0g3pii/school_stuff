f0d812f86320c8f67f7db05b15b7311d5e1921ae
nicoh2 <49153387+t0g3pii@users.noreply.github.com>
2021-10-25 14:43:58 +0200

    yeet

--- Bankdaten/Bankdaten/Rechenwerk.cs
+++ Bankdaten/Bankdaten/Rechenwerk.cs
@@ -12,34 +12,40 @@ namespace Bankdaten
 		/// <returns></returns>
 		public int BerechnePruefsumme(int a, int b)
 		{
+			Console.WriteLine("Block 1");
 			int x;
 			int y;
 			int z;
 
 			if (a < b)
 			{
+				Console.WriteLine("Block 2");
 				x = b;
 				z = 1;
 			}
 			else
 			{
+				Console.WriteLine("Block 3");
 				x = a;
 				z = 0;
 			}
 
 			if ((a + b) < 10)
 			{
+				Console.WriteLine("Block 4");
 				y = x + b / z;
 			}
 			else
 			{
+				Console.WriteLine("Block 5");
 				y = x + a;
 				if ((a + b) == 7)
 				{
+					Console.WriteLine("Block 6");
 					y++;
 				}
 			}
-
+			Console.WriteLine("Block 7");
 			return x + y + z;
 		}
 
