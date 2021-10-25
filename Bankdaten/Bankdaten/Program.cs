d29051ac6c523c69b21e24539a241ebd295476e9
nicoh2 <49153387+t0g3pii@users.noreply.github.com>
2021-10-25 14:07:43 +0200

    Added Bankdaten from Use Case Tests

---
+++ Bankdaten/Bankdaten/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Bankdaten
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Rechenwerk rechenwerk = new Rechenwerk();
+
+			Console.WriteLine("PRÜFSUMMENGENERATOR");
+			Console.WriteLine("Geben Sie Ihre Abteilungs- und Betriebsnummer ein!");
+			Console.WriteLine("Abteilungsnummer: ");
+			int abteilungNr = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Betriebsnummer:");
+			int betriebNr = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine(rechenwerk.BerechnePruefsumme(abteilungNr, betriebNr));
+			Console.ReadKey();
+
+
+		}
+	}
+}
---
+++ Bankdaten/Bankdaten/Rechenwerk.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Bankdaten
+{
+	public class Rechenwerk
+	{
+		/// <summary>
+		/// Berechnet eine Prüfsumme aus Eingabewerten.
+		/// </summary>
+		/// <param name="a">Abteilungsnummer</param>
+		/// <param name="b">Betriebsnummer</param>
+		/// <returns></returns>
+		public int BerechnePruefsumme(int a, int b)
+		{
+			int x;
+			int y;
+			int z;
+
+			if (a < b)
+			{
+				x = b;
+				z = 1;
+			}
+			else
+			{
+				x = a;
+				z = 0;
+			}
+
+			if ((a + b) < 10)
+			{
+				y = x + b / z;
+			}
+			else
+			{
+				y = x + a;
+				if ((a + b) == 7)
+				{
+					y++;
+				}
+			}
+
+			return x + y + z;
+		}
+
+	}
+}
