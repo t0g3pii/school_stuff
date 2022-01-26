2e6df7097e1523f6a2b14bd4d263634eb26d45fb
nicoh2 <49153387+t0g3pii@users.noreply.github.com>
2022-01-26 11:58:13 +0100

    Added Frogger to school_stuff (init)

---
+++ Frogger/Frogger/FrmFrogger.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Frogger
+{
+    public partial class FrmFrogger : Form
+    {
+        // Deklarationen
+
+        // Die Anzahl an Bereichen auf dem Spielfeld.
+        static int anzahlBereiche = 6;
+
+        // Variablen für Dimensionen
+        // Wenn eine Variable den Wert "-1" hat, wurde sie noch nicht mit den richtigen Werten gefüllt.
+        int breite = -1;
+        int hoehe = -1;
+        int hoeheJeBereich = -1;
+
+        // Array für alle Bereiche (Start, Bahnen, Ziel)
+        Rectangle[] alleBahnen = new Rectangle[anzahlBereiche];
+
+        // Listen für die Hindernisse auf den Bahnen
+        List<Hindernis> alleHindernisse = new List<Hindernis>();
+
+        Rectangle spieler;
+
+        int spawnRate = 14;
+        int spawnZaehler = 0;
+
+        // Zufalszahlengenerator
+        Random rndBahn = new Random();
+
+        // TODO Spieler darf nicht nach unten aus dem Spielfeld laufen
+        // TODO Weitere Bewegungen (links, rechts) einbauen
+        // TODO Wenn Spieler im Ziel ist, soll er wieder auf Start zurückgesetzt werden und das Spiel soll schwerer werden
+        // TODO Kontrollieren, ob Spieler überfahren wurde.
+
+
+        public FrmFrogger()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmFrogger_Load(object sender, EventArgs e)
+        {
+            DoubleBuffered = true;
+        }
+
+        private void FrmFrogger_Paint(object sender, PaintEventArgs e)
+        {
+
+            if(tmrGameTick.Enabled == false)
+            {
+
+                breite = this.ClientSize.Width;
+
+                hoehe = this.ClientSize.Height;
+
+                hoeheJeBereich = hoehe / anzahlBereiche;
+
+
+                hoeheJeBereich = hoeheJeBereich + 2;
+
+
+                spieler = new Rectangle((breite / 2) - 15, hoehe - 35, 30, 30);
+
+                for (int i = 0; i < alleBahnen.Length; i++)
+                {
+                    alleBahnen[i] = new Rectangle(0, i * hoeheJeBereich, breite, hoeheJeBereich);
+                }
+
+                tmrGameTick.Start();
+            }
+
+
+
+            SolidBrush brStart = new SolidBrush(Color.LightBlue);
+            SolidBrush brZiel = new SolidBrush(Color.LightYellow);
+            SolidBrush brBahnHell = new SolidBrush(Color.LightGray);
+            SolidBrush brBahnDunkel = new SolidBrush(Color.Gray);
+            SolidBrush brSpieler = new SolidBrush(Color.Green);
+            Pen pnRand = new Pen(Color.Black, 1);
+
+
+            e.Graphics.FillRectangles(brBahnHell, alleBahnen);
+
+            // TODO Bahnen sollen sich in der Farbe abwechseln (brBahnHell, brBahnDunkel)
+
+            e.Graphics.FillRectangle(brZiel, alleBahnen[0]);
+            e.Graphics.FillRectangle(brStart, alleBahnen[alleBahnen.Length-1]);
+            
+
+            e.Graphics.DrawRectangles(pnRand, alleBahnen);
+
+
+            foreach(Hindernis aktuellesHindernis in alleHindernisse)
+            {
+                e.Graphics.FillRectangle(
+                    aktuellesHindernis.Brush,
+                    aktuellesHindernis.X,
+                    aktuellesHindernis.Y,
+                    aktuellesHindernis.Width,
+                    aktuellesHindernis.Height);
+            }
+
+
+            e.Graphics.FillEllipse(brSpieler, spieler);
+
+
+        }
+
+        private void tmrGameTick_Tick(object sender, EventArgs e)
+        {
+
+            spawnZaehler++;
+            if(spawnZaehler == spawnRate)
+            {
+                spawnZaehler = 0;
+
+
+                int zufall = rndBahn.Next(1, anzahlBereiche-1);
+                int yWertDerBahn = alleBahnen[zufall].Top;
+
+                alleHindernisse.Add(new Hindernis(breite, yWertDerBahn, 60, hoeheJeBereich, 10, Color.Red));
+            }
+
+
+            foreach (Hindernis aktuellesHindernis in alleHindernisse)
+            {
+                aktuellesHindernis.Move();
+            }
+
+            for(int i = alleHindernisse.Count -1; i >= 0; i--)
+            { 
+                if ((alleHindernisse[i].X + alleHindernisse[i].Width) < 0)
+                {
+                    alleHindernisse.RemoveAt(i);
+
+                }
+            }
+
+            this.Refresh();
+        }
+
+        private void FrmFrogger_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Up)
+            {
+                spieler.Y = spieler.Y - hoeheJeBereich;
+            }
+
+            if(e.KeyCode == Keys.Down)
+            {
+                spieler.Y = spieler.Y + hoeheJeBereich;
+            }
+
+            this.Refresh();
+        }
+    }
+}
